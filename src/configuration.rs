3521f759dfec4e97b21a824165c9cd7e7f2f7e03
Danylo Kovalenko <danil130999@gmail.com>
2022-09-03 21:24:53 +0300

    fix: Add missing files

---
+++ src/configuration.rs
@@ -0,0 +1,28 @@
+
+static CONFIG_PATH: &str = "config.yaml";
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16
+}
+
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String
+}
+
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let settings = config::Config::builder()
+        .add_source(config::File::new(
+            CONFIG_PATH, 
+            config::FileFormat::Yaml))
+        .build()?;
+    settings.try_deserialize::<Settings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::net::TcpListener;
 
 use img_service::startup::run;
-
+use img_service::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind rand port");
     run(listener)?.await
